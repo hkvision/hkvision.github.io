835223215fb2d75a96a80ad78e3a512934d80453
hkvision <huangkaivision@gmail.com>
2017-07-12 16:21:21 +0800

    Website
    New style!

--- handleMessage.php
+++
@@ -1,7 +0,0 @@
-<?php
-$name = $_GET['name'];
-$email = $_GET['email'];
-$msg = $_GET['message'];
-mail("huangkaivision@gmail.com","From: ".$name." (".$email.")",$msg);
-echo 'Thank you so much for your message. Look forward to my reply soon!';
-?>
