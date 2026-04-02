29752f26941ade978ebf415aaec02e2bfe2b2215
Huang <kai.k.huang@intel.com>
2026-04-02 14:56:25 +0800

    专辑信息placeholder

--- assets/script.js
+++ assets/script.js
@@ -114,13 +114,77 @@
     $(this).toggleClass('open');
   });
 
-  // 打开视频 modal 时加载 iframe，关闭时停止播放
+  // 专辑曲目数据（后续替换真实链接）
+  var albumTracks = [
+    { title: 'Track 01', audioIndex: 0, qqUrl: 'https://y.qq.com/n/ryqq_v2/songDetail/0039tIo02YCRDk', wyyUrl: null, bvid: 'BV1DT4y1U7qk', biliUrl: 'https://www.bilibili.com/video/BV1DT4y1U7qk' },
+    { title: 'Track 02', audioIndex: 1, qqUrl: 'https://y.qq.com/', wyyUrl: null,                     bvid: null, biliUrl: null },
+    { title: 'Track 03', audioIndex: 2, qqUrl: 'https://y.qq.com/', wyyUrl: null,                     bvid: null, biliUrl: null },
+    { title: 'Track 04', audioIndex: 3, qqUrl: 'https://y.qq.com/', wyyUrl: null,                     bvid: null, biliUrl: null },
+    { title: 'Track 05', audioIndex: 4, qqUrl: 'https://y.qq.com/', wyyUrl: null,                     bvid: null, biliUrl: null },
+    { title: 'Track 06', audioIndex: 5, qqUrl: 'https://y.qq.com/', wyyUrl: null,                     bvid: null, biliUrl: null },
+    { title: 'Track 07', audioIndex: 6, qqUrl: 'https://y.qq.com/', wyyUrl: null,                     bvid: null, biliUrl: null },
+    { title: 'Track 08', audioIndex: 7, qqUrl: 'https://y.qq.com/', wyyUrl: null,                     bvid: null, biliUrl: null },
+    { title: 'Track 09', audioIndex: 8, qqUrl: 'https://y.qq.com/', wyyUrl: null,                     bvid: null, biliUrl: null },
+    { title: 'Track 10', audioIndex: 9, qqUrl: 'https://y.qq.com/', wyyUrl: 'https://music.163.com/', bvid: null, biliUrl: null }
+  ];
+
+  // 渲染专辑曲目列表
+  $.each(albumTracks, function(i, t) {
+    var num = (i + 1 < 10 ? '0' : '') + (i + 1);
+    var titleHtml = t.qqUrl
+      ? '<a class="album-track-link" href="' + t.qqUrl + '" target="_blank">' + t.title + '</a>'
+      : (t.wyyUrl ? '<a class="album-track-link" href="' + t.wyyUrl + '" target="_blank">' + t.title + '</a>' : t.title);
+    var actions = '';
+    actions += '<button class="btn-play-track" data-audio-index="' + t.audioIndex + '">&#9654;</button>';
+    if (t.bvid) actions += '<button class="btn-mv" data-bvid="' + t.bvid + '" data-title="' + t.title + '"><i class="fa fa-film"></i> MV</button>';
+    $('#album-track-list').append(
+      '<li class="album-track-item">' +
+      '<span class="album-track-num">' + num + '</span>' +
+      '<span class="album-track-title">' + titleHtml + '</span>' +
+      '<span class="album-track-actions">' + actions + '</span>' +
+      '</li>'
+    );
+  });
+
+  // 专辑内点击播放 → 底部播放器（保持弹窗不关闭）
+  $(document).on('click', '.btn-play-track', function() {
+    var idx = parseInt($(this).data('audio-index'));
+    loadSong(idx);
+    audio.play();
+    isPlaying = true;
+    setPlayIcon();
+  });
+
+  // 专辑内点击 MV → 关闭专辑弹窗后打开视频弹窗
+  var pendingBvid = null;
+  var pendingTitle = null;
+  $(document).on('click', '.btn-mv', function() {
+    pendingBvid  = $(this).data('bvid');
+    pendingTitle = $(this).data('title');
+    $('#album-modal').modal('hide');
+  });
+  var videoFromAlbum = false;
+  $('#album-modal').on('hidden.bs.modal', function() {
+    if (pendingBvid) {
+      videoFromAlbum = true;
+      $('#video-modal-title').text(pendingTitle + ' - MV');
+      $('#bilibili-player').data('bvid', pendingBvid);
+      $('#video-modal').modal('show');
+      pendingBvid = null;
+    }
+  });
+
+  // 视频 modal：打开时加载 iframe，关闭时停止；若从专辑进入则返回专辑
   $('#video-modal').on('show.bs.modal', function() {
-    var player = $('#bilibili-player');
-    player.attr('src', player.data('src'));
+    var bvid = $('#bilibili-player').data('bvid');
+    if (bvid) $('#bilibili-player').attr('src', 'https://player.bilibili.com/player.html?bvid=' + bvid + '&page=1');
   });
   $('#video-modal').on('hidden.bs.modal', function() {
     $('#bilibili-player').attr('src', '');
+    if (videoFromAlbum) {
+      videoFromAlbum = false;
+      $('#album-modal').modal('show');
+    }
   });
  
     $(".scroll a, .navbar-brand, .gototop").click(function(event){   
