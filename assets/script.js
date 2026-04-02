d762fd7fd299e69421ef65ce5868fd53f1692f67
Huang <kai.k.huang@intel.com>
2026-04-02 14:31:05 +0800

    加播放器

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,119 @@
  jQuery(document).ready(function($) {
 
+  // 音乐播放器
+  var songs = [
+    { title: 'Track 01', file: 'audio/01.mp3' },
+    { title: 'Track 02', file: 'audio/02.mp3' },
+    { title: 'Track 03', file: 'audio/03.mp3' },
+    { title: 'Track 04', file: 'audio/04.mp3' },
+    { title: 'Track 05', file: 'audio/05.mp3' },
+    { title: 'Track 06', file: 'audio/06.mp3' },
+    { title: 'Track 07', file: 'audio/07.mp3' },
+    { title: 'Track 08', file: 'audio/08.mp3' },
+    { title: 'Track 09', file: 'audio/09.mp3' },
+    { title: 'Track 10', file: 'audio/10.mp3' }
+  ];
+
+  var currentIndex = 0;
+  var isPlaying = false;
+  var audio = document.getElementById('audio-player');
+
+  // 渲染歌单
+  $.each(songs, function(i, song) {
+    $('#player-list').append(
+      '<li class="player-list-item' + (i === 0 ? ' active' : '') + '" data-index="' + i + '">' +
+      '<span class="player-list-num">' + (i + 1 < 10 ? '0' : '') + (i + 1) + '</span>' +
+      '<span class="player-list-title">' + song.title + '</span>' +
+      '<i class="fa fa-play player-list-icon"></i>' +
+      '</li>'
+    );
+  });
+
+  function loadSong(index) {
+    currentIndex = index;
+    audio.src = songs[index].file;
+    $('.player-title').text(songs[index].title);
+    $('.player-list-item').removeClass('active');
+    $('.player-list-item[data-index="' + index + '"]').addClass('active');
+    $('.player-progress-bar').css('width', '0%');
+    $('.player-current').text('0:00');
+    $('.player-duration').text('0:00');
+  }
+
+  function formatTime(s) {
+    var m = Math.floor(s / 60);
+    var sec = Math.floor(s % 60);
+    return m + ':' + (sec < 10 ? '0' : '') + sec;
+  }
+
+  function setPlayIcon() {
+    $('#player-play i').attr('class', 'fa fa-pause');
+    $('.player-list-item[data-index="' + currentIndex + '"] .player-list-icon')
+      .attr('class', 'fa fa-pause player-list-icon');
+  }
+
+  function setPauseIcon() {
+    $('#player-play i').attr('class', 'fa fa-play');
+    $('.player-list-icon').attr('class', 'fa fa-play player-list-icon');
+  }
+
+  loadSong(0);
+
+  $('#player-play').click(function() {
+    if (isPlaying) {
+      audio.pause();
+      isPlaying = false;
+      setPauseIcon();
+    } else {
+      audio.play();
+      isPlaying = true;
+      setPlayIcon();
+    }
+  });
+
+  $('#player-prev').click(function() {
+    loadSong((currentIndex - 1 + songs.length) % songs.length);
+    if (isPlaying) { audio.play(); setPlayIcon(); }
+  });
+
+  $('#player-next').click(function() {
+    loadSong((currentIndex + 1) % songs.length);
+    if (isPlaying) { audio.play(); setPlayIcon(); }
+  });
+
+  $(audio).on('timeupdate', function() {
+    if (audio.duration) {
+      $('.player-progress-bar').css('width', (audio.currentTime / audio.duration * 100) + '%');
+      $('.player-current').text(formatTime(audio.currentTime));
+      $('.player-duration').text(formatTime(audio.duration));
+    }
+  });
+
+  $(audio).on('ended', function() {
+    loadSong((currentIndex + 1) % songs.length);
+    audio.play();
+    setPlayIcon();
+  });
+
+  $('.player-progress').click(function(e) {
+    if (audio.duration) {
+      audio.currentTime = (e.offsetX / $(this).width()) * audio.duration;
+    }
+  });
+
+  $(document).on('click', '.player-list-item', function() {
+    loadSong(parseInt($(this).data('index')));
+    audio.play();
+    isPlaying = true;
+    setPlayIcon();
+  });
+
+  // 歌单展开/收起
+  $('#player-toggle').click(function() {
+    $('#music-playlist').toggleClass('open');
+    $(this).toggleClass('open');
+  });
+
   // 打开视频 modal 时加载 iframe，关闭时停止播放
   $('#video-modal').on('show.bs.modal', function() {
     var player = $('#bilibili-player');
